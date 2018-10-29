9ee11b2e734663b7b7d80d7e8a3047f68f0eb2aa
Esperanza Dueñas <da13002@ues.edu.sv>
2018-10-29 11:19:20 -0600

    login funcionando con google

--- lib/vistas/sign_in.dart
+++ lib/vistas/sign_in.dart
@@ -1,59 +1,159 @@
+import 'dart:async';
+import 'dart:convert' show json;
+import "package:http/http.dart" as http;
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-class SignIn extends StatefulWidget{
+
+GoogleSignIn _googleSignIn = GoogleSignIn(
+  scopes: <String>[
+    'email',
+    'https://www.googleapis.com/auth/contacts.readonly',
+  ],
+);
+
+
+
+class SignIn extends StatefulWidget {
   @override
-  State<StatefulWidget> createState(){
-    return new BotonSignIn();
-  }
+  State createState() => SignInState();
 }
 
-class BotonSignIn extends  State<SignIn>{
+class SignInState extends State<SignIn> {
+  GoogleSignInAccount _currentUser;
+  String _contactText;
 
-  signIn(){}
   @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new Stack(
+  void initState() {
+    super.initState();
+    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount account) {
+      setState(() {
+        _currentUser = account;
+      });
+      if (_currentUser != null) {
+        _handleGetContact();
+      }
+    });
+    _googleSignIn.signInSilently();
+  }
+
+  Future<void> _handleGetContact() async {
+    setState(() {
+      _contactText = "Loading contact info...";
+    });
+    final http.Response response = await http.get(
+      'https://people.googleapis.com/v1/people/me/connections'
+          '?requestMask.includeField=person.names',
+      headers: await _currentUser.authHeaders,
+    );
+    if (response.statusCode != 200) {
+      setState(() {
+        _contactText = "People API gave a ${response.statusCode} "
+            "response. Check logs for details.";
+      });
+      print('People API ${response.statusCode} response: ${response.body}');
+      return;
+    }
+    final Map<String, dynamic> data = json.decode(response.body);
+    final String namedContact = _pickFirstNamedContact(data);
+    setState(() {
+      if (namedContact != null) {
+        _contactText = "I see you know $namedContact!";
+      } else {
+        _contactText = "No contacts to display.";
+      }
+    });
+  }
+
+  String _pickFirstNamedContact(Map<String, dynamic> data) {
+    final List<dynamic> connections = data['connections'];
+    final Map<String, dynamic> contact = connections?.firstWhere(
+      (dynamic contact) => contact['names'] != null,
+      orElse: () => null,
+    );
+    if (contact != null) {
+      final Map<String, dynamic> name = contact['names'].firstWhere(
+        (dynamic name) => name['displayName'] != null,
+        orElse: () => null,
+      );
+      if (name != null) {
+        return name['displayName'];
+      }
+    }
+    return null;
+  }
+
+  Future<void> _handleSignIn() async {
+    try {
+      await _googleSignIn.signIn();
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  Future<void> _handleSignOut() async {
+    _googleSignIn.disconnect();
+  }
+
+  Widget _buildBody() { 
+    if (_currentUser != null) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          ListTile(
+            leading: GoogleUserCircleAvatar(
+              identity: _currentUser,
+            ),
+            title: Text(_currentUser.displayName),
+            subtitle: Text(_currentUser.email),
+          ),
+          const Text("Signed in successfully."),
+          Text(_contactText),
+          RaisedButton(
+            child: const Text('Salir'),
+            onPressed: _handleSignOut,
+          ),
+          RaisedButton(
+            child: const Text('Refrescar'),
+            onPressed: _handleGetContact,
+          ),
+        ],
+      );
+    } else {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           new Container(
+            alignment: new Alignment(0.0 ,0.0),
+            child: RaisedButton.icon(
+              onPressed: _handleSignIn,
+              textColor: Colors.black,
+              color: Colors.white,
+              icon: new Icon(FontAwesomeIcons.google, color: Colors.redAccent),
+              label: new Text(
+                " Entrar con Google",
+                style: new TextStyle(
+                color: Colors.black
+                )
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: new Stack(
+        children:<Widget>[
+          new Container(
             decoration: new BoxDecoration(
-              image: new DecorationImage(image: new AssetImage("images/fondo.jpg"), fit: BoxFit.cover,),
+              image: new DecorationImage(image: new AssetImage("images/fondo.jpg"), fit: BoxFit.cover),
             ),
           ),
-          new Center(
-            child: new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                new Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    new RaisedButton.icon(
-                      onPressed: signIn(),
-                      textColor: Colors.black,
-                      color: Colors.black,
-                      icon: new Icon(FontAwesomeIcons.google, color: Colors.redAccent),
-                      label: new Text("Entrar con Google     ", 
-                        style: new TextStyle(
-                          color: Colors.black
-                        ),
-                      ),
-                    ),
-                    new RaisedButton.icon(
-                      onPressed: signIn(),
-                      textColor: Colors.black,
-                      color: Colors.black,
-                      icon: new Icon(FontAwesomeIcons.facebook, color: Colors.blueAccent,),
-                      label: new Text("Entrar con Facebook",
-                        style: new TextStyle(
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
+          _buildBody(),
         ],
       ),
     );
